e0d35ac3fbe20d092b4eeffa04630dff2e67b953
waterstonemf <waterstonemf@outlook.com>
2015-02-09 20:50:29 +0800

    Test Git Hub
    First Project to upload to git

---
+++ TestGitHub/TestGitHub/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Hello Git!");
+            Console.Read();
+        }
+    }
+}
