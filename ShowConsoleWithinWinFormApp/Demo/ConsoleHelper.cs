1edc91b8e0f48addc22e44eaf5a57efedaf6afb2
waterstonemf <waterstonemf@outlook.com>
2015-03-10 17:46:10 +0800

    ShowConsoleInWinForm
    Add Demo project

---
+++ ShowConsoleWithinWinFormApp/Demo/ConsoleHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Demo
+{
+    class ConsoleHelper
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool AllocConsole();
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+
+
+        public static void ShowConsoleWindow()
+        {
+            var handle = GetConsoleWindow();
+
+            if (handle == IntPtr.Zero)
+            {
+                AllocConsole();
+            }
+            else
+            {
+                ShowWindow(handle, SW_SHOW);
+            }
+        }
+
+        public static void HideConsoleWindow()
+        {
+            var handle = GetConsoleWindow();
+
+            ShowWindow(handle, SW_HIDE);
+        }
+
+
+    }
+}
---
+++ ShowConsoleWithinWinFormApp/Demo/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+#if DEBUG
+            ConsoleHelper.ShowConsoleWindow();
+#endif
+            System.Diagnostics.Trace.WriteLine("Form1_load");
+#if DEBUG
+            Console.Out.WriteLine("This message should be printed on console!");
+#endif
+        }
+    }
+}
