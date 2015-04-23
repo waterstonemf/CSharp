43fb089c334b285ad19ff255dcdecce4859c9e77
waterstonemf <waterstonemf@outlook.com>
2015-04-23 15:28:07 +0800

    Modify

--- NQueen/NQueen/Program.cs
+++ NQueen/NQueen/Program.cs
@@ -10,8 +10,21 @@ namespace NQueen
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Backtrack");
             Queen q = new Queen();
             q.Test();
+
+            Console.WriteLine("Recursive Queen2");
+            Queen2 q2 = new Queen2();
+            q2.Test();
+
+            Console.WriteLine("Recursive Queen3");
+            Queen3 q3 = new Queen3();
+            q3.Test();
+
+            Console.WriteLine("Recursive Queen4");
+            Queen4 q4 = new Queen4();
+            q4.Test();
 
             Console.Read();
         }
