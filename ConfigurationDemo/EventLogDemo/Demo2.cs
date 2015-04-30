eeff2af1a684271d55e1fdb617c635f5710e914e
waterstonemf <waterstonemf@outlook.com>
2015-04-30 11:06:58 +0800

    AddEventLogDemo

---
+++ ConfigurationDemo/EventLogDemo/Demo2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace EventLogDemo
+{
+    class Demo2
+    {
+        public void test()
+        {
+            EventLog m_EventLog = new EventLog("");
+            m_EventLog.Source = "EventLogDemo_Demo2";
+            m_EventLog.WriteEntry("Any log information",
+                EventLogEntryType.FailureAudit);
+        }
+    }
+}
